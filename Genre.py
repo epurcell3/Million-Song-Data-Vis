feeaea295684ebe4ba663ab73637a25d877874f2
Ryan <rmccaffrey3@gatech.edu>
2013-11-04 10:30:03 -0500

    fixed to work with songlist

---
+++ Genre.py
@@ -0,0 +1,18 @@
+__author__ = 'Ryan'
+class Genre:
+    def __init__(self, keyword, initialSong):
+        self.keyword = keyword
+        self.minimalList = initialSong.terms
+        self.songCount = 0
+        self.children = []
+        self.rank = len(initialSong.keywords)
+
+    def addSong(self, song):
+        self.songCount += 1
+        self.minListAdjust(song.terms)
+
+    def minListAdjust(self, newList):
+        for currents in self.minimalList:
+            if (currents not in newList):
+                self.minimalList.remove(currents)
+                self.rank = len(self.minimalList)
--- GenreBase.py
+++ GenreBase.py
@@ -1,15 +1,16 @@
 __author__ = 'Ryan'
+import Genre
 class GenreBase:
-    def __init__(self, songBase):
+    def __init__(self, songList):
         self.fulllist = {}
         self.zeroslist = []
         notEmpty = True
         while notEmpty:
-            currentSong = songBase.getNext()
+            currentSong = songList.getNext()
             if currentSong is None:
                 notEmpty = False
             else:
-                for keyword in currentSong.keywords:
+                for keyword in currentSong.terms:
                     if self.fulllist[keyword] is None:
                         self.fulllist[keyword] = Genre(keyword, currentSong)
                     else:
@@ -33,29 +34,8 @@ class GenreBase:
         return maxKey
 
 
-class Genre:
-    def __init__(self, keyword, initialSong):
-        self.keyword = keyword
-        self.minimalList = initialSong.keywords
-        self.songCount = 0
-        self.children = []
-        self.rank = len(initialSong.keywords)
-
-    def addSong(self, song):
-        self.songCount += 1
-        self.minListAdjust(song.keywords)
-
-    def minListAdjust(self, newList):
-        for currents in self.minimalList.keys():
-            if (newList[currents] is None):
-                del self.minimalList[currents]
-                self.rank = len(self.minimalList)
 
 
-
-class Song:
-    def __init__(self, keywords):
-        self.keywords = keywords
 
 
 
