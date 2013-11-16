ca8bd15641263ea3a96048f7ac857ba79643cb4a
epurcell3 <epurcell3@gatech.edu>
2013-11-16 16:03:58 -0500

    Fixed inner circles being too far away
    Added a scale to CircleInCircle.java.

--- Java/src/visualizations/CircleInCircle.java
+++ Java/src/visualizations/CircleInCircle.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class CircleInCircle
 {
 	public int x, y, r;
+	public double scale = 0.01;
 	public ArrayList<CircleInCircle> innerCircles;
 
 	public CircleInCircle(int x, int y, int r)
@@ -26,7 +27,7 @@ public class CircleInCircle
 		int offset = 0;
 		for(CircleInCircle c: this.innerCircles)
 		{
-			c.x = this.x - this.r + offset + c.r;
+			c.x = this.x - (int)(this.r * scale) + offset + c.r;
 			offset += 2*c.r;
 		}
 	}
