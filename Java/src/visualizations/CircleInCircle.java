6578b069ebbbaf77048f0f0bafa58f70b06c3804
epurcell3 <epurcell3@gatech.edu>
2013-11-19 09:53:17 -0500

    Changed the inner circles to make a circle.
    There aren't any inner circles now though? Not sure what's up with that.

--- Java/src/visualizations/CircleInCircle.java
+++ Java/src/visualizations/CircleInCircle.java
@@ -1,6 +1,9 @@
 package visualizations;
 
 import java.util.ArrayList;
+import java.util.Random;
+
+import java.lang.Math;
 
 import processing.core.PApplet;
 import processing.core.PConstants;
@@ -13,6 +16,10 @@ public class CircleInCircle implements Drawable
 	public double scale;
 	public ArrayList<CircleInCircle> innerCircles;
 	public int red, grn, blu;
+	
+	private double angle = Math.PI/2;
+	private double d;
+	private double BUFFER = 5 * 2*Math.PI/180;
 
 	public CircleInCircle(PApplet parent, int x, int y, int r, int red, int grn, int blu, double scale)
 	{
@@ -26,7 +33,7 @@ public class CircleInCircle implements Drawable
 		this.blu = blu;
         highlighted = false;
 		this.scale = scale;
-
+		this.d = 2 / 3 * this.r;
 	}
 	
 	public CircleInCircle(CircleVis parent, int x, int y, int r)
@@ -36,8 +43,19 @@ public class CircleInCircle implements Drawable
 
 	public void addCircle(int r)
 	{
-		this.innerCircles.add(new CircleInCircle(parent, this.x, this.y, r, 0, 255, 0, 0.05));
-		pack();
+		this.angle += Math.acos((2*Math.pow(this.d, 2)-Math.pow(r*0.05, 2))/2*Math.pow(this.d, 2));
+		if (this.angle > 2 * Math.PI)
+		{
+			this.angle = Math.PI/2;
+			this.d = this.r / 2;
+		}
+		
+		int x = (int)(Math.cos(this.angle) * this.d * this.scale + this.x);
+		int y = (int)(Math.sin(this.angle) * this.d * this.scale + this.y);
+		this.innerCircles.add(new CircleInCircle(parent, x, y, r, 0, 255, 0, 0.05));
+		
+		this.angle += Math.acos((2*Math.pow(this.d, 2)-Math.pow(r*0.05, 2))/2*Math.pow(this.d, 2)) + BUFFER;
+		//pack();
 	}
 	
 	public void draw()
@@ -50,6 +68,7 @@ public class CircleInCircle implements Drawable
         	c.draw();
         }
 	}
+	
     public boolean highlight(int mx, int my){
         boolean isHighlighted = false;
         double xs = (x-mx) * (x-mx);
@@ -69,6 +88,7 @@ public class CircleInCircle implements Drawable
         }
         return isHighlighted;
     }
+    
     public boolean highlightInners(int mx, int my){
         boolean current = false;
         for(int i = 0; i < innerCircles.size(); i++){
@@ -79,13 +99,42 @@ public class CircleInCircle implements Drawable
         }
         return  current;
     }
+    
 	private void pack()
 	{
-		int offset = 0;
-		for(CircleInCircle c: this.innerCircles)
+		Random rand = new Random();
+		double angle = Math.PI/2;
+		double increment = 2*Math.PI/this.innerCircles.size();
+		
+		for (int i = 0; i < this.innerCircles.size(); i++)
 		{
-			c.x = (int)((this.x - (this.r*scale) + offset + (c.r*c.scale)));
-			offset += 2*c.r*c.scale;
+			boolean placed = true;
+			do {
+				CircleInCircle c = this.innerCircles.get(i);
+				c.x = this.x;
+				c.y = this.y;
+				//double angle = rand.nextDouble() * 2 * Math.PI;
+				double radius = rand.nextInt((int)(this.r*this.scale - c.r*c.scale));
+				
+				int x = (int)(Math.cos(angle) * radius);
+				int y = (int)(Math.sin(angle) * radius);
+				
+				for (int j = 0; j < i; j++)
+				{
+					CircleInCircle c2 = this.innerCircles.get(j);
+					if ((Math.pow(x - c2.x, 2) + Math.pow(y - c2.y, 2)) < Math.pow(c2.r * c2.scale, 2))
+					{
+						placed = false;
+					}
+				}
+				if (placed)
+				{
+					c.x = x;
+					c.y = y;
+				}
+			} while(!placed);
+			//c.x = (int)((this.x - (this.r*scale) + offset + (c.r*c.scale)));
+			//offset += 2*c.r*c.scale;
 		}
 	}
 }
