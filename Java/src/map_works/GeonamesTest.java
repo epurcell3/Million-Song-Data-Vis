514ff6eeaa32d93ec0ac705dacfb5e942153d314
mcdc09 <tbowling3@res364d-128-61-71-209.res.gatech.edu>
2013-11-20 15:48:57 -0500

    Simple test for using GeoNames WebService

---
+++ Java/src/map_works/GeonamesTest.java
@@ -0,0 +1,28 @@
+package map_works;
+
+import java.io.IOException;
+
+import org.geonames.WebService;
+
+/**
+ * Simple test for using Geonames lookups with our database
+ * @author tbowling3
+ *
+ */
+public class GeonamesTest {
+	
+	public static void main(String[] args) {
+		WebService.setUserName("mcdc09");
+		
+		//String searchName = "http://api.geonames.org/countryCode?lat=47.03&lng=10.2&username=mcdc09"; // the string we want to lookup in geonames database
+		try {
+			String country = WebService.countryCode(35.00, 80.00);
+			System.out.println("Country: " + country);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+} // close GeonamesTest
+
+
