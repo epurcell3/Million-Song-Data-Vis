d8019d44cf441f9904425ed548e6d213d9cd678f
mcdc09 <tbowling3@res364d-128-61-71-209.res.gatech.edu>
2013-11-20 15:48:13 -0500

    Added Country and Continent data to DB File
    Needed to be run only once.

---
+++ Java/src/database/AddLocationDataToDB.java
@@ -0,0 +1,82 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+
+import map_works.ContinentData;
+import backend.Artist;
+import backend.PathHandler;
+import backend.SongList;
+
+public class AddLocationDataToDB {
+	public static String dbTotal = "MillionSongSubset.db";
+	private String dbPath;
+
+	public AddLocationDataToDB() {
+		
+	} // close constructor
+	
+	public void performLocationUpdate() {
+		PathHandler ph = new PathHandler();
+		dbPath = ph.getPathToResource(dbTotal);
+
+		Connection conOut = null;
+
+		try {
+			Class.forName("org.sqlite.JDBC").newInstance();
+
+			conOut = DriverManager
+					.getConnection("jdbc:sqlite:" + dbPath);
+
+			if (!conOut.isClosed())  System.out.println("Successfully connected to database");
+			
+			Statement state = conOut.createStatement();
+			//state.execute("ALTER TABLE artists_h5 ADD COLUMN artist_country text"); // ALREADY DONE
+			
+			DatabaseConnectionTotal dct = new DatabaseConnectionTotal();
+			SongList sl = dct.getArtistLocation();
+			Collection<Artist> artists = sl.getArtists();
+			
+			
+			System.out.println();
+			System.out.println("Update Queries:");
+			
+			ContinentData cd = new ContinentData();
+			for(Artist a: artists) {
+				String a_id = a.getArtist_id();
+				double a_lat = a.getArtist_latitude();
+				double a_lon = a.getArtist_longitude();
+				String country = cd.getCountryForLocation(a_lat, a_lon);
+				String continent = cd.getContinentForCountry(country);
+				
+				String updateSQL = createUpdateSQL(a_id, continent, country);
+				System.out.println(updateSQL);
+				state.execute(updateSQL);
+			}
+			
+			
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+		} finally {
+
+		} // close finally
+	}
+	
+	public String createUpdateSQL(String a_id, String continent, String country) {
+		return "UPDATE artists_h5 SET artist_continent = '" + continent + "', artist_country = '" + country + "' WHERE artist_id = '" + a_id + "';";
+	}
+
+	public static void main(String[] args) {
+		AddLocationDataToDB ald = new AddLocationDataToDB();
+		ald.performLocationUpdate();
+
+	}
+
+}
