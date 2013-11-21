95205f5f89225077907721a7c3e605191f08cb31
mcdc09 <tbowling3@r1-128-61-71-209.res.gatech.edu>
2013-11-21 10:42:11 -0500

    ArtistLocationApplet now shows a set of four checkboxes - NOT COMPLETE
    This is a rough outline of using checkboxes.  Dynamic filtering needs
    to be added, and the checkboxes need to be moved where they are shown
    in the visualization.

--- Java/src/visualizations/ArtistLocationApplet.java
+++ Java/src/visualizations/ArtistLocationApplet.java
@@ -3,6 +3,7 @@ package visualizations;
 import processing.core.PApplet;
 import backend.SongList;
 import codeanticode.glgraphics.GLConstants;
+import controlP5.ControlEvent;
 import database.DatabaseConnectionTotal;
 
 public class ArtistLocationApplet extends PApplet {
@@ -12,6 +13,8 @@ public class ArtistLocationApplet extends PApplet {
 	 */
 	private static final long serialVersionUID = 464355466041128604L;
 	private ArtistLocationMap alm;
+	private static boolean USE_CHECKBOX = true;
+	private ContinentCheckboxes ccbx;
 	private String artistText = "";
 
 	public void setup() {
@@ -21,15 +24,26 @@ public class ArtistLocationApplet extends PApplet {
 		SongList sl = dct.getArtistLocation();
 		alm = new ArtistLocationMap(this, sl);
 		
+		if (USE_CHECKBOX) ccbx = new ContinentCheckboxes(this);
+		
 	}
 	
 	public void mouseMoved() {
 		alm.mouseMoved(mouseX, mouseY);
 	}
 	
+	public void keyPressed() {
+		if (USE_CHECKBOX) ccbx.keyPressed();
+	}
+	
+	public void controlEvent(ControlEvent theEvent) {
+		if (USE_CHECKBOX) ccbx.controlEvent(theEvent);
+	}
+	
 	public void draw() {
 		fill(255,255,0);
 		alm.draw();
+		if (USE_CHECKBOX) ccbx.draw();
 		
 		fill(50);
 		text(artistText, 50, 50);
---
+++ Java/src/visualizations/ContinentCheckboxes.java
@@ -0,0 +1,85 @@
+package visualizations;
+
+import map_works.ContinentData;
+import processing.core.PApplet;
+import controlP5.CheckBox;
+import controlP5.ControlEvent;
+import controlP5.ControlP5;
+
+public class ContinentCheckboxes {
+	private PApplet parent;
+	private ControlP5 cp5;
+	private CheckBox checkbox;
+
+	public ContinentCheckboxes(PApplet p) {
+		this.parent = p;
+		String[] continents = ContinentData.getContinents();
+
+		cp5 = new ControlP5(parent);
+		checkbox = cp5.addCheckBox("checkBox")
+				.setPosition(100, 200)
+				.setColorForeground(parent.color(120))
+				.setColorActive(parent.color(255))
+				.setColorLabel(parent.color(255))
+				.setSize(40, 40)
+				.setItemsPerRow(3)
+				.setSpacingColumn(30)
+				.setSpacingRow(20)
+				.addItem(continents[0], 0)
+				.addItem(continents[1], 50)
+				.addItem(continents[2], 100)
+				.addItem(continents[3], 150)
+				.addItem(continents[4], 200)
+				.addItem(continents[5], 255)
+				;
+	} // close constructor
+
+	public void keyPressed() {
+		if (parent.key==' ') {
+			checkbox.deactivateAll();
+		} 
+		else {
+			for (int i=0;i<6;i++) {
+				// check if key 0-5 have been pressed and toggle
+				// the checkbox item accordingly.
+				if (parent.keyCode==(48 + i)) { 
+					// the index of checkbox items start at 0
+					checkbox.toggle(i);
+					System.out.println("toggle "+checkbox.getItem(i).getLabel());
+					// also see 
+					// checkbox.activate(index);
+					// checkbox.deactivate(index);
+				}
+			}
+		}
+	}
+
+	public void draw() {
+		parent.pushMatrix();
+		parent.translate(parent.width/2 + 200, parent.height/2);
+		parent.stroke(255);
+		parent.strokeWeight(2);
+		parent.popMatrix();
+	}
+
+	void controlEvent(ControlEvent theEvent) {
+		if (theEvent.isFrom(checkbox)) {
+			System.out.print("got an event from "+checkbox.getName()+"\t\n");
+			// checkbox uses arrayValue to store the state of 
+			// individual checkbox-items. usage:
+			System.out.println(checkbox.getArrayValue());
+			for (int i=0;i<checkbox.getArrayValue().length;i++) {
+				int n = (int)checkbox.getArrayValue()[i];
+				System.out.print(n);
+				if(n==1) {
+//					myColorBackground += checkbox.getItem(i).internalValue();
+				}
+			}
+			System.out.println();    
+		}
+	}
+
+	void checkBox(float[] a) {
+		System.out.println(a);
+	}
+}
