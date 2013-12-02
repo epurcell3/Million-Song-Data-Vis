21f52bffc41fa13f5949f36ceeab49a3e525e1da
mcdc09 <ThomasBowling@Comcast.net>
2013-12-02 16:47:53 -0500

    Created AbstractMap Object and extended to maps

---
+++ Java/src/visualizations/AbstractMap.java
@@ -0,0 +1,23 @@
+package visualizations;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+
+public abstract class AbstractMap {
+	
+	protected UnfoldingMap map;
+	
+	public abstract void draw();
+	
+	protected boolean contains(float x1, float y1, double cx, double cy, double radius) {
+		boolean out = false;
+		if(distance(x1, y1, cx, cy) <= radius) {
+			out = true;
+		}
+		return out;
+	}
+	
+	protected double distance(double x1, double y1, double x2, double y2) {
+		return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+	}
+
+}
---
+++ Java/src/visualizations/GenreLocationMap.java
@@ -0,0 +1,33 @@
+package visualizations;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.geo.Location;
+import processing.core.PApplet;
+
+public class GenreLocationMap extends AbstractMap {
+	private PApplet parent;
+	private int x, y, width, height;
+	
+	public GenreLocationMap(PApplet p, int x, int y, int width, int height) {
+		this.parent = p;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		
+		map = new UnfoldingMap(parent, "detail", x, y, width, height);
+	}
+	
+	public void draw() {
+		map.draw();
+		
+		
+	}
+	
+	public Location getMapLocation(int mouseX, int mouseY) {
+		return map.getLocation(mouseX, mouseY);
+	}
+	
+	
+
+}
